2ba3399304bd5ee138215c0be4d24efb225f5fa7
Patrick MacDonald <patrick@patmac.ca>
2026-02-20 19:41:28 -0700

    set up remote images with google

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
 	cacheComponents: true,
+	images:{
+		remotePatterns:[{
+			protocol:'https',
+			hostname:'https://lh3.googleusercontent.com',
+			pathname:'*'
+		}],
+		
+	}
 }
 
 export default nextConfig
