0af6d8ff1533e90d04ee595fec54ff95453562ba
Patrick MacDonald <patrick@patmac.ca>
2026-02-20 19:05:17 -0700

    added correct redirect

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -27,7 +27,8 @@ export async function signInWithGoogle() {
 	const { data, error } = await supabase.auth.signInWithOAuth({
 		provider: 'google',
 		options: {
-			redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/auth/callback`,
+			redirectTo:
+				'https://lsovuldxbdzrevyzwztg.supabase.co/auth/v1/callback',
 		},
 	})
 
